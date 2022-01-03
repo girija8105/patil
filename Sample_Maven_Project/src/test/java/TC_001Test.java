a252e3ad14ef34ce10b3485aa6af11bade9fa2e5
girrija <girrija@DESKTOP-61HEHPO>
2022-01-03 21:36:07 +0530

    push to github

---
+++ Sample_Maven_Project/src/test/java/TC_001Test.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class TC_001Test {
+	
+	@Test
+	public void mobileShop() {
+		System.out.println("print mobileShop");
+	}
+	
+	@Test
+	public void webShop() {
+		System.out.println("print webshop");
+	}
+
+}
---
+++ Sample_Maven_Project/src/test/java/TC_002Test.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TC_002Test {
+	
+	@Test
+	public void repairShop() {
+		System.out.println("repair shop");
+	}
+
+}
